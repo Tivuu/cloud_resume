8318a68b7e1e42ea7cb8bb950f5aed84c2025197
Tivuu <rdr4264@gmail.com>
2025-07-09 10:27:39 -0600

    branch to test cloudflare as cdn instead

--- main.tf
+++ main.tf
@@ -15,18 +15,9 @@ provider "aws" {
 
 resource "aws_s3_bucket" "mathproblemguy" {
   bucket = "mathproblemguy.com"
-
-  tags = {
-    Name        = "mathproblemguy.com"
-    Environment = "Dev"
-  }
 }
 
-resource "aws_s3_bucket" "wwwmathproblemguy" {
-  bucket = "www.mathproblemguy.com"
-}
-
-resource "aws_s3_bucket_website_configuration" "mathproblemguy" {
+resource "aws_s3_bucket_website_configuration" "site" {
   bucket = aws_s3_bucket.mathproblemguy.id
 
   index_document {
@@ -38,76 +29,27 @@ resource "aws_s3_bucket_website_configuration" "mathproblemguy" {
   }
 }
 
-resource "aws_s3_bucket_policy" "cloudfront_access" {
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket = aws_s3_bucket.mathproblemguy.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.mathproblemguy.id
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "AllowCloudFrontAccess"
-        Effect    = "Allow"
-        Principal = {
-          "Service" : "cloudfront.amazonaws.com"
-        }
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.mathproblemguy.arn}/*"
-        Condition = {
-          StringEquals = {
-            "AWS:SourceArn" = aws_cloudfront_distribution.mathproblemguy.arn
-          }
-        }
-      }
-    ]
+    Statement = [{
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.mathproblemguy.arn}/*"
+    }]
   })
 }
 
-resource "aws_cloudfront_origin_access_control" "mathproblemguy_oac" {
-  name                              = "mathproblemguy-oac"
-  description                       = "OAC for mathproblemguy CloudFront to access S3"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
-
-resource "aws_cloudfront_distribution" "mathproblemguy" {
-  enabled             = true
-  default_root_object = "index.html"
-
-  origin {
-    domain_name = aws_s3_bucket.mathproblemguy.bucket_regional_domain_name
-    origin_id   = "S3-mathproblemguy"
-
-    origin_access_control_id = aws_cloudfront_origin_access_control.mathproblemguy_oac.id
-  }
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-mathproblemguy"
-
-    viewer_protocol_policy = "redirect-to-https"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  tags = {
-    Environment = "Production"
-  }
-}
