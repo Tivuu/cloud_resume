6869a36cb86be168ba86922a625d9df15e163837
Tivuu <rdr4264@gmail.com>
2025-07-09 17:22:47 -0600

    adding base html and subdomain for future use

--- main.tf
+++ main.tf
@@ -53,3 +53,43 @@ resource "aws_s3_bucket_policy" "public_read" {
   })
 }
 
+resource "aws_s3_bucket" "gnocchimapmathproblemguy" {
+  bucket = "gnocchimap.mathproblemguy.com"
+}
+
+resource "aws_s3_bucket_website_configuration" "subsite" {
+  bucket = aws_s3_bucket.gnocchimapmathproblemguy.id
+
+  index_document {
+    suffix = "/build/index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "sub_allow_public" {
+  bucket = aws_s3_bucket.gnocchimapmathproblemguy.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "sub_public_read" {
+  bucket = aws_s3_bucket.gnocchimapmathproblemguy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid       = "PublicReadGetObject"
+      Effect    = "Allow"
+      Principal = "*"
+      Action    = "s3:GetObject"
+      Resource  = "${aws_s3_bucket.gnocchimapmathproblemguy.arn}/*"
+    }]
+  })
+}
+
